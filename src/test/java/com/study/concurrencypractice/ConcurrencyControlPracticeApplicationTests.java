302e252c3b2e0e6197ded3b6e23adccb986362c4
이재민 <jaemin@MacBookPro.local>
2023-06-08 23:17:36 +0900

    initial commit

---
+++ src/test/java/com/study/concurrencypractice/ConcurrencyControlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.concurrencypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyControlPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
